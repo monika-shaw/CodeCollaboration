bc56c147c01b83c96554b4db905e9b9ea1a2447c
shawmonika616ms <shawmonika616ms@gmail.com>
2024-03-31 11:04:59 +0530

    Description : working on room logic. Developer : Monika

--- client/src/routes/room/Room.js
+++ client/src/routes/room/Room.js
@@ -4,8 +4,26 @@ import AceEditor from 'react-ace'
 function Room() {
   return (
     <div>
-      <div className='roomSideBar'></div>
-      <AceEditor/>
+      <div className='roomSideBar'>
+        <div className='roomsidearWrapper'>
+          <div className='languageFieldWrapper'>
+            <select className='languageField' name='language' id='language'></select>
+          </div>
+          <div className='languageFieldWrapper'>
+            <select className='languageField' name='language' id='language'></select>
+          </div>
+          <p>Connected Users:</p>
+          <div className='roomSidebarUsers'>
+
+          </div>
+
+          <button>Copy Room Id</button>
+          <button>Leave Room</button>
+        </div>
+      </div>
+      <AceEditor
+        placeholder='Write your code here'
+      />
     </div>
   )
 }
