723e585d84c461e688a6cf5e9165b6f1a466b90d
shawmonika616ms <shawmonika616ms@gmail.com>
2024-04-01 23:08:46 +0530

    Description  : imported and install ace builds.

--- client/src/routes/room/Room.js
+++ client/src/routes/room/Room.js
@@ -1,7 +1,22 @@
 import React from 'react'
 import AceEditor from 'react-ace'
 
+import "ace-builds/src-noconflict/mode-java";
+import "ace-builds/src-noconflict/mode-javascript";
+import "ace-builds/src-noconflict/mode-typescript";
+import "ace-builds/src-noconflict/mode-python";
+import "ace-builds/src-noconflict/mode-yaml";
+import "ace-builds/src-noconflict/mode-golang";
+import "ace-builds/src-noconflict/mode-html";
+import "ace-builds/src-noconflict/mode-css";
+import "ace-builds/src-noconflict/mode-csharp";
+import "ace-builds/src-noconflict/mode-c_cpp";
+
 function Room() {
+
+  const copyToClipBoard = () => {
+
+  }
   return (
     <div>
       <div className='roomSideBar'>
@@ -17,7 +32,7 @@ function Room() {
 
           </div>
 
-          <button>Copy Room Id</button>
+          <button onClick={copyToClipBoard}>Copy Room Id</button>
           <button>Leave Room</button>
         </div>
       </div>
