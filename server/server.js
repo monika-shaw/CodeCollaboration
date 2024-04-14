3d6a098de0135abedb614404ebd6a9b1746d7fc9
shawmonika616ms <shawmonika616ms@gmail.com>
2024-04-14 10:11:11 +0530

    Description : socket.io connection completed. Developer : Monika.

--- server/server.js
+++ server/server.js
@@ -5,15 +5,30 @@ const cors = require('cors')
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server)
+const io = new Server(server, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST']
+    }
+})
+
 app.use(cors)
 
 app.get('/', function (req, res) {
     res.send("Hello from the server")
 })
 
+
+const socket_ID_USERS = {}
+const roomID_USERS = {}
+
+io.on('connection', function(socket){
+    
+})
+
+
 const PORT = 5000;
 
-server.listen(PORT, function(){
+server.listen(PORT, function () {
     console.log("Hello ");
 })
